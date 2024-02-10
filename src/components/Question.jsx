e34c078b195f37b53181eeb9c402d57bc5bf4c72
MonaRuz <mona.ruz@seznam.cz>
2024-02-11 00:24:25 +0100

    Only one answer in a question list can be shown

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,23 +1,28 @@
-import { useState } from "react"
 import { Link } from "react-router-dom"
 import styles from "./Question.module.css"
 import Button from "./Button"
-import { IoIosArrowDown,IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io"
 
-export default function Question({ id, question, answer }) {
-	const [isActive, setIsActive] = useState(false)
-	console.log(id)
+export default function Question({
+	id,
+	question,
+	answer,
+	isOpen,
+	onToggleOpen,
+}) {
 	return (
 		<div>
 			<li className={question}>
 				<div
 					className={styles.questionBox}
-					onClick={() => setIsActive(!isActive)}
+					onClick={() => onToggleOpen()}
 				>
 					<p className={styles.question}>{question}</p>
-					<p className={styles.btn}>{isActive ? <IoIosArrowUp/> : <IoIosArrowDown/>}</p>
+					<p className={styles.btn}>
+						{isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+					</p>
 				</div>
-				{isActive && (
+				{isOpen && (
 					<div>
 						<p className={styles.answer}>{answer}</p>
 						<div className={styles.btnBox}>
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
 import styles from "./Questions.module.css"
 import SearchQuestion from "./SearchQuestion"
 import Question from "./Question"
 
 export default function Questions({ questions }) {
 	const [searchQuery, setSearchQuery] = useState("")
+	const[isOpen,setIsOpen]=useState(null)
+
+	function handlToggleOpen(id){
+        setIsOpen(id === isOpen ? null : id)
+    }
+	
 	const searchedQuestions =
 		searchQuery.length > 0
 			? questions.filter((question) =>
@@ -19,7 +24,7 @@ export default function Questions({ questions }) {
 			<SearchQuestion searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
 			<ul className={styles.list}>
 				{searchedQuestions.map((question) => (
-					<Question key={question.id} question={question.question} answer={question.answer} id={question.id}/>
+					<Question key={question.id} question={question.question} answer={question.answer} id={question.id} isOpen={question.id===isOpen} onToggleOpen={()=>handlToggleOpen(question.id)}/>
 				))}
 			</ul>
 		</div>
