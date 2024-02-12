d82a9cf5431964327e6ae8b1ff8f2bea28b5edf4
MonaRuz <mona.ruz@seznam.cz>
2024-02-12 20:05:42 +0100

    Deleted useTest hook

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,20 +1,55 @@
+import { useEffect, useState } from "react"
 import Button from "./Button"
 import styles from "./Test.module.css"
-import useTest from "../hooks/useTest"
-import { useQuestions } from "../contexts/QuestionsContext"
+import {useQuestions} from "..//contexts/QuestionsContext"
 
 export default function Test() {
+	const [showAnswer, setShowAnswer] = useState(false)
+	const [percentCouter, setpercentCouter] = useState(0)
+	const [progress, setProgress] = useState(1)
+	const [currentQuestion, setCurrentQuestion] = useState(null)
 
-	const { questions, testQuestions } = useQuestions()
-	const {
-		showAnswer,
-		setShowAnswer,
-		progress,
-		currentQuestion,
-		result,
-		handleWrongAnswer,
-		handleCorrectAnswer,
-	} = useTest()
+	const{questions,testQuestions,setTestQuestions}=useQuestions()
+
+	const result = Math.ceil(
+		100 - (percentCouter / (questions.length + percentCouter)) * 100
+	)
+
+	function getRandomQuestion(min, max) {
+		min = Math.ceil(min)
+		max = Math.floor(max)
+		return Math.floor(Math.random() * (max - min) + min)
+	}
+
+	function handleWrongAnswer() {
+		setCurrentQuestion(
+			testQuestions[getRandomQuestion(0, testQuestions.length)]
+		)
+		setShowAnswer(false)
+		setpercentCouter(percentCouter + 1)
+	}
+
+	function handleCorrectAnswer(id) {
+		let updatedQuestions = testQuestions.filter((question) => {
+			return question.id !== id
+		})
+		setTestQuestions(updatedQuestions)
+
+		setShowAnswer(false)
+		setCurrentQuestion(
+			updatedQuestions[getRandomQuestion(0, updatedQuestions.length)]
+		)
+		setProgress(progress + 1)
+	}
+
+	useEffect(
+		function () {
+			setCurrentQuestion(
+				testQuestions[getRandomQuestion(0, testQuestions.length)]
+			)
+		},
+		[testQuestions]
+	)
 
 	if (testQuestions.length !== 0)
 		return (
@@ -73,7 +108,6 @@ export default function Test() {
 				</p>
 			</div>
 		)
-		
 	if (testQuestions.length === 0)
 		return <p className='result'>Úspěšnost testu byla {result}%</p>
 }
